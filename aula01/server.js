16f84874c84e292d72ca797c3ccd164153565321
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-09-15 19:55:54 -0300

    ajustes arquivos aula 01

--- aula01/server.js
+++ aula01/server.js
@@ -2,10 +2,11 @@
 const http = require('http');
 
 // criar a funcao generica que vai responder 
-//as requisicoes do servidor
-const tratarRespostaDaRequisicao = function(requisicao, resposta){
+// as requisicoes do servidor
+const tratarRespostaDaRequisicao = function(_, resposta){
     resposta.writeHead(200, { 'Content-Type' : 'text/html'});
     resposta.write("<h1>Meu Primeiro Servidor Web em Node.js</h1>");
+    resposta.end();
 }
 
 // executa a funcao createServer do http que espera um tratamento 
@@ -14,6 +15,6 @@ const server = http.createServer(tratarRespostaDaRequisicao);
 
 // executa o servidor na porta 3000 e notifica no console quando 
 // o servidor terminar de subir
-server.listen(3000, function(){
-    console.log('Servidor no ar na porta 3000');
+server.listen(3200, function(){
+    console.log('Servidor no ar na porta 3200');
 });
